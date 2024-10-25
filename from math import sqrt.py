53338cb708bc556af1ec1abf031b88e1f574efb6
Kuzmich27 <akuzmichev27@gmail.com>
2024-10-25 21:08:00 +0300

    Add files via upload

---
+++ from math import sqrt.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+x_int = 10
+y_int = 5
+
+
+def add_numbers(x_int, y_int):
+    return int(x_int) + int(y_int)
+
+
+def CalculateSquareRoot(Number):
+    return sqrt(Number)
+
+
+def calc(your_number):
+    if your_number <= 0:
+        your_number = 0
+    total = CalculateSquareRoot(your_number)
+    tot = (f'Мы вычислили квадратный корень из введённого Вами числа.'
+           f'Это будет: {total}'
+           )
+    return tot
+
+
+print('Сумма чисел:', add_numbers(x_int, y_int))
+print(calc(25.5))
